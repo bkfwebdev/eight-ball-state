5a390802aa670d30e0c1b99cfd1dc1eb4d584755
bkfwebdev <bryant.feld@gmail.com>
2021-09-27 16:45:37 -0400

    created basic component

---
+++ src/EightBall.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EightBall(props) {
+    return (
+        <div className = "eight-ball">
+            <p>Think of A Question</p>
+        </div>
+    )
+}

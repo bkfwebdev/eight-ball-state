ccf2104b8cae7869f1f63ff5f5e48d06463176ae
bkfwebdev <bryant.feld@gmail.com>
2021-09-27 22:57:37 -0400

    basic functionality complete

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function EightBall(props) {
+    const [message,setMessage] = useState("Think of a question");
+    const [bgColor,setbgColor] = useState("black");
+    const handleClick = ()=> {
+        console.log(props);
+        
+        let myRandomNum = Math.floor(Math.random() * 20);
+        setMessage(props.props[myRandomNum].msg);
+        setbgColor(props.props[myRandomNum].color);
+    
+    }
+
     return (
-        <div className = "eight-ball">
-            <p>Think of A Question</p>
+        <div>
+        <div className = "eight-ball"
+        style = {{backgroundColor : bgColor}}>
+            <p>{message}</p>
         </div>
+        <button onClick = {handleClick}>click me</button>
+        </div>
+       
     )
 }
+ 
